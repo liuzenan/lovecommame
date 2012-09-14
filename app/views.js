e6d2e299850d962a9fc99008bf76a589e175f7d5
Liu Zenan <liuzenan711@gmail.com>
2012-09-14 08:57:42 +0800

    add templates

--- app/views.js
+++ app/views.js
@@ -3,16 +3,19 @@ define([
   "zepto",
   "lodash",
   "backbone",
-
+  "text!templates/tpl_login.html",
+  "text!templates/tpl_postcard_display.html",
+  "text!templates/tpl_postcard_edit.html",
+  "text!templates/tpl_wall.html"
 ],
 
-function($, _, Backbone){
+function($, _, Backbone,tpl_login,tpl_postcard_display,tpl_postcard_edit,tpl_wall){
 
 	var V = V || {};
 
 	//login view
 	V.Login = Backbone.View.extend({
-
+		
 	});
 
 	//wall view
@@ -20,19 +23,40 @@ function($, _, Backbone){
 
 	});
 
+
 	//postcard edit mode
-	V.PostCard_Edit = Backbone.View.extend({
+	V.PostcardEdit = Backbone.View.extend({
 
 	});
 
+
+	//edit mode stage 1 -- write message
+	V.PostcardEditWrite = Backbone.View.extend({
+
+	});
+
+	//edit mode stage 2 -- upload/take photo
+	V.PostcardEditPhoto = Backbone.View.extend({
+
+	});
+
+	//edit mode stage 3 -- add photo effect
+	V.PostcardEditEffect = Backbone.View.extend({
+
+	});
+
+	//edit mode stage 4 -- send postcard
+	V.PostcardEditSend = Backbone.View.extend({
+
+	});
 
 	//postcard display mode
-	V.PostCard_Display = Backbone.View.extend({
+	V.PostcardDisplay = Backbone.View.extend({
 
 	});
 
 
-	
+
 
 	return V;
 
