ece43e61db5b6f1799d545501667b52bfe0a93d1
Scott Meng <blackridermkz@gmail.com>
2012-09-24 09:55:09 +0800

    adding post request for signin and signup

--- app/modules/user.js
+++ app/modules/user.js
@@ -22,8 +22,32 @@ function(app, Backbone){
 		},
 
 		login: function(ev){
+			//signin();
 			app.router.go("wall");
 			return false; 
+		},
+
+		signin: function(ev){
+			// temperarily disable all inputs during the ajex request
+			var $inputs = $("form").find("input, select, button, textarea");
+			$inputs.attr("disabled", "disabled");
+
+			// uploading user credential to user for verification
+			$.ajex({
+				url: 'ec2-54-251-19-5.ap-southeast-1.compute.amazonaws.com/api.php/user',
+				type: 'post',
+				dataType: 'json',
+				data: {Username: window.btoa($("form").username), Password: window.btoa($("form").password)},
+				
+				success: function(data){
+					alert("successful");
+					// check if a token is returned
+				},
+				error: function(){
+					alert("error!");
+					// for debugging purposes
+				}
+			});
 		}
 	});
 
@@ -36,7 +60,7 @@ function(app, Backbone){
 		},
 
 		signup: function(ev){
-			register();
+			//register();
 			app.router.go("wall"); // or should it go to a re-direct page?
 			return false;
 		},
@@ -48,13 +72,14 @@ function(app, Backbone){
 		},
 
 		register: function(){
-			// upload user credentials to the server
-			var serializedData = $("#signup").serialize();
-			var $inputs = $("#signup").find("input, select, button, textarea");
+			// serialize user credential data
+			var serializedData = $("form").serialize();
+			var $inputs = $("form").find("input, select, button, textarea");
 
 			// temperarily disable all inputs during the ajex request
 			$inputs.attr("disabled", "disabled");
 
+			// uploading user credential form to the server
 			$.ajex({
 				url: 'ec2-54-251-19-5.ap-southeast-1.compute.amazonaws.com/api.php/user',
 				type: 'post',
