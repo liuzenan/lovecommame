06412ec689565237393cb9303c7a3a5bf3ccfef8
Liu Zenan <liuzenan711@gmail.com>
2012-09-28 21:40:36 +0800

    update style

--- app/modules/postcard.js
+++ app/modules/postcard.js
@@ -346,11 +346,14 @@ Postcard.Views.DraftList = Postcard.Views.List.extend({
 
     changeStyle: function(ev){
       var currentbutton = ev.target;
-      console.log(currentbutton);
-      var currentStyle = $(ev.targert).attr("id");
-      $(".create.postcard.front.face").removeClass("style1").removeClass("style2").removeClass("style3").removeClass("style4").addClass(currentStyle);
-      $(".stamp").removeClass("style1").removeClass("style2").removeClass("style3").removeClass("style4").addClass(currentStyle);
-      $(".chop").removeClass("style1").removeClass("style2").removeClass("style3").removeClass("style4").addClass(currentStyle);
+      var currentStyle = $(currentbutton).attr("id");
+      console.log(currentStyle);
+      $(".create.postcard.front.face").removeClass("style1 style2 style3 style4")
+      $(".create.postcard.front.face").addClass(currentStyle);
+      $(".stamp").removeClass("style1 style2 style3 style4");
+      $(".stamp").addClass(currentStyle);
+      $(".chop").removeClass("style1 style2 style3 style4");
+      $(".chop").addClass(currentStyle);
     },
 
     saveNew: function(ev){
